2f92a84393ec0f482b9a286b24a64097c7740465
Fernando Torterolo <fernando.torterolo@autodesk.com>
2015-03-11 16:52:33 -0300

    add some packages for futures activities

---
+++ src/org/ucu/teoria2/actividad4/actividad4.java
@@ -0,0 +1,12 @@
+package org.ucu.teoria2.actividad4;
+
+/**
+ * Actividad 1, Teorica de la Computación y sistemas formales.
+ * Grupo 6 - Pancreas.
+ * 9/3/2015
+ * Integrantes: Rodrigo Suarez, Sebastian Caredio, Fernando Torterolo, Gianni Iaquinta, José Ignacio Chartier.
+ * Descripcion:
+ *
+ */
+public class actividad4 {
+}
